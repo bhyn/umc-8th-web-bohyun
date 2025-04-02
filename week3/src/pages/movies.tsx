10307d741d1e76743c4ab3c4b9e9691b3f7fbc45
김보현 <10amykim@naver.com>
2025-04-03 00:00:02 +0900

    데이터를 불러왔어!!

--- week3/src/pages/movies.tsx
+++ week3/src/pages/movies.tsx
@@ -1,12 +1,42 @@
-import {useParams} from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Movie, MovieResponse } from '../types/movie';
+
+import axios from 'axios';
 
 const MoviesPage = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
 
-    const params = useParams();
-    console.log(params);
-    return (
-        <h1>{params.movieId}번의 Movies Page 야호~!</h1>
-    );
+  console.log(movies);
+
+  useEffect(() => {
+    
+    const fetchMovies = async () => {
+      const { data } = await axios.get<MovieResponse>(
+        `https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1`,
+        {
+          headers: {
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNjI0YjZjOWU4NDllYjBmYTJkOTAwMDJiYWY0YzZkNCIsIm5iZiI6MTc0MzYwMzg4NS4yMjgsInN1YiI6IjY3ZWQ0OGFkNGVkNWI1MjcyMWNlNThlMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.b-SXEMnqLIOjNQwhnNty9cCHZCMWtqtjAV8tCJ8IXB0`,
+          },
+        }
+      );
+
+      setMovies(data.results);
+    };
+
+    fetchMovies();
+  }, []);
+
+  return (
+    <ul>
+      {/* 옵셔널 체인 활용 */}
+      {movies?.map((movie) => (
+        <li key={movie.id}>
+          <h1>{movie.title}</h1>
+        </li>
+      ))}
+    </ul>
+  );
+  
 };
 
 export default MoviesPage;
