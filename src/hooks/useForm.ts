da8ce1d6cb0a93cf66d6933f18fe81ba8f678b09
김보현 <10amykim@naver.com>
2025-05-28 17:53:13 +0900

    loginpagee

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -7,13 +7,12 @@ interface UseFormProps<T> {
 
 function useForm<T>({ initialValue, validate }: UseFormProps<T>) {
   const [values, setValues] = useState(initialValue);
-  const [touched, setTouched] = useState<Record<string, boolean>>({});
-  const [errors, setErrors] = useState<Record<string, string>>({});
+  const [touched, setTouched] = useState<Record<string, boolean>>();
+  const [errors, setErrors] = useState<Record<string, string>>();
 
-  // 사용자 입력값을 변경하는 함수
   const handleChange = (name: keyof T, text: string) => {
     setValues({
-      ...values, // 기존 상태 유지
+      ...values,
       [name]: text,
     });
   };
@@ -25,10 +24,8 @@ function useForm<T>({ initialValue, validate }: UseFormProps<T>) {
     });
   };
 
-  // 이메일, 패스워드 입력 요소들, 속성들을 좀 가져오는 것
   const getInputProps = (name: keyof T) => {
     const value = values[name];
-
     const onChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>
       handleChange(name, e.target.value);
 
@@ -37,11 +34,12 @@ function useForm<T>({ initialValue, validate }: UseFormProps<T>) {
     return { value, onChange, onBlur };
   };
 
-  // values가 변경될 때마다 에러 검증 로직이 실행됨.
   useEffect(() => {
-    const newErrors: Record<keyof T, string> = validate(values);
-    setErrors(newErrors); // 오류 메시지 입력
+    const newErrors = validate(values);
+    setErrors(newErrors);
   }, [validate, values]);
 
   return { values, errors, touched, getInputProps };
 }
+
+export default useForm;
