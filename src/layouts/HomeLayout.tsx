57245f2a0df2645a9d3f6b2ad2d143822fa050fc
김보현 <10amykim@naver.com>
2025-05-28 15:47:10 +0900

    로그인 화면 구현

--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -1,15 +1,54 @@
-import {Outlet} from 'react-router-dom';
+import { Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const HomeLayout = () => {
-    return (
-        <div className='h-dvh flex flex-col'>
-            <nav>내브바</nav>
-            <main className='flex-1'>
-                <Outlet />
-            </main>
-            <footer>푸터</footer>
+  return (
+    <div className="h-dvh flex flex-col">
+      <nav className="flex justify-between items-center p-4 bg-gray-50">
+        <div className="space-x-2">
+          <>
+            <Link
+              to="/login"
+              className="bg-gray-200 text-white px-4 py-2 rounded-md hover:bg-gray-600 transition-all mx-1"
+            >
+              로그인
+            </Link>
+            <Link
+              to="/signup"
+              className="bg-gray-200 text-white px-4 py-2 rounded-md hover:bg-gray-600 transition-all mx-1"
+            >
+              회원가입
+            </Link>
+          </>
+
+          <Link
+            to="/"
+            className="bg-gray-200 text-white px-4 py-2 rounded-md hover:bg-gray-600 transition-all mx-1"
+          >
+            홈페이지
+          </Link>
+
+          <Link
+            to="/my"
+            className="bg-gray-200 text-white px-4 py-2 rounded-md hover:bg-gray-600 transition-all mx-1"
+          >
+            마이페이지
+          </Link>
+
+          <Link
+            to={"/search"}
+            className="text-gray-700 dark:text-gray-300 hover:text-blue-500"
+          >
+            검색하시오!
+          </Link>
         </div>
-    );
+      </nav>
+      <main className="flex-1">
+        <Outlet /> //여기에 자식 컴포넌트를 보여줘
+      </main>
+      <footer>푸터</footer>
+    </div>
+  );
 };
 
 export default HomeLayout;
